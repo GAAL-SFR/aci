a5021ebc510fc0582f97cab45c9bbc93bdf3c6c2
GAAL-SFR <97745726+GAAL-SFR@users.noreply.github.com>
2022-01-14 18:16:40 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
-provider "aws" {
-  region = "eu-west-3"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+
+  }
+
+  required_version = ">= 0.14.0"
 }
 
 data "aws_ami" "ubuntu" {
