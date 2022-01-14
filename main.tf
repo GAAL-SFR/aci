feb32a4a48bf89bf1ef3ca589d5fd13de6ae7e53
GAAL-SFR <97745726+GAAL-SFR@users.noreply.github.com>
2022-01-14 17:02:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
 }
 
 provider "aws" {
